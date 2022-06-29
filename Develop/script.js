0c92340d09f721fe7699f77e464082adbfae9599
zzcipod <jonzzheng@gmail.com>
2022-06-28 23:16:17 -0700

    make function logical

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -17,7 +17,7 @@ var optionArray = [];
 
 
 function pinlength () {
- pinNumbers = parseInt(alert("How many chracters would you want to input? The characters limits is between 8 and 128"));
+ pinNumbers = parseInt(prompt("How many chracters would you want to input? The characters limits is between 8 and 128"));
 
  if(pinNumbers<8 || pinNumbers>128){
    alert("The characters limits is between 8 and 128 ")
@@ -41,7 +41,7 @@ function pinlength () {
  console.log(specialWords);
  console.log(numeWords);
  console.log(lowerWords);
- console.log(upperWordsl);
+ console.log(upperWords);
  ConTogether();
 
  if (!specialWords && !numeWords && !lowerWords && !upperWords) {
@@ -72,28 +72,31 @@ if (upperWordsl) {
       }
       
 console.log(possibleChars)
-var password = arrayLoop();
+var password = PassLoop();
 var passwordText = document.querySelector("#password");
 passwordText.value =password;
     }
 
-function arrayLoop(pinNumbers) {
+function PassLoop() {
+  finalArray=[];
   for (let i = 0; i < pinNumbers; i++) {
         finalArray = finalArray.concat(possibleChars[Math.floor(Math.random() * possibleChars.length)]);
   }
-  console.log(finalArray)
+  console.log(pinNumbers);
+  console.log(finalArray);
   return finalArray.join ("");
 }
 
 
-let pinNumbers = validateUserInput()
-userConfirm()
-generateArray()
-var result = arrayLoop(pinNumbers)
+// let pinNumbers = validateUserInput()
+// userConfirm()
+// generateArray()
+// var result = arrayLoop(pinNumbers)
 
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function pinlength() {
+function CreatePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
